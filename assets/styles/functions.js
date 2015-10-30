bd17058fc6547e17e989867c4cbfd2a994f54c53
Hudson Nicoletti <hudson19962009@gmail.com>
2015-10-30 16:55:46 -0200

    Initial Commit

---
+++ assets/styles/functions.js
@@ -0,0 +1,20 @@
+(function($){
+    
+    $(document).ready(function(){
+        
+        var $window           = $(window),
+            $slider           = $('.slides ul'),
+            SliderConfig      = {
+                autoPlay : true,
+                navigation : false,
+                slideSpeed : 300,
+                pagination : false,
+                paginationSpeed : 400,
+                singleItem: true,
+                //stopOnHover: true,
+                addClassActive: false
+            };
+        
+    });
+    
+})(jQuery)
